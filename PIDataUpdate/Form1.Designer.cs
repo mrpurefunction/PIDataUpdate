e4efd1e8dda7a654ceaf743cc633aca3d4cfe15b
mrpurefunction <mrpurefunction@sina.com>
2015-08-20 15:51:22 +0800

    add

---
+++ PIDataUpdate/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace PIDataUpdate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.PointListBox = new System.Windows.Forms.CheckedListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.StartdateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.EnddateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.realupdate_btn = new System.Windows.Forms.Button();
+            this.avgupdate_btn = new System.Windows.Forms.Button();
+            this.realstatuslabel = new System.Windows.Forms.Label();
+            this.avgstatuslabel = new System.Windows.Forms.Label();
+            this.bothupdate_btn = new System.Windows.Forms.Button();
+            this.stopupdate_btn = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.UpdateprogressBar = new System.Windows.Forms.ProgressBar();
+            this.label7 = new System.Windows.Forms.Label();
+            this.percentagelabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "计量点:";
+            // 
+            // PointListBox
+            // 
+            this.PointListBox.CheckOnClick = true;
+            this.PointListBox.FormattingEnabled = true;
+            this.PointListBox.Location = new System.Drawing.Point(15, 25);
+            this.PointListBox.Name = "PointListBox";
+            this.PointListBox.Size = new System.Drawing.Size(199, 292);
+            this.PointListBox.TabIndex = 1;
+            this.PointListBox.ThreeDCheckBoxes = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(236, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "起始时间:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(236, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "结束时间:";
+            // 
+            // StartdateTimePicker
+            // 
+            this.StartdateTimePicker.CustomFormat = "yyyy-MM-dd HH:mm:00";
+            this.StartdateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.StartdateTimePicker.Location = new System.Drawing.Point(301, 9);
+            this.StartdateTimePicker.Name = "StartdateTimePicker";
+            this.StartdateTimePicker.Size = new System.Drawing.Size(204, 21);
+            this.StartdateTimePicker.TabIndex = 4;
+            // 
+            // EnddateTimePicker
+            // 
+            this.EnddateTimePicker.CustomFormat = "yyyy-MM-dd HH:mm:00";
+            this.EnddateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.EnddateTimePicker.Location = new System.Drawing.Point(301, 56);
+            this.EnddateTimePicker.Name = "EnddateTimePicker";
+            this.EnddateTimePicker.Size = new System.Drawing.Size(204, 21);
+            this.EnddateTimePicker.TabIndex = 5;
+            // 
+            // realupdate_btn
+            // 
+            this.realupdate_btn.Location = new System.Drawing.Point(227, 117);
+            this.realupdate_btn.Name = "realupdate_btn";
+            this.realupdate_btn.Size = new System.Drawing.Size(91, 23);
+            this.realupdate_btn.TabIndex = 6;
+            this.realupdate_btn.Text = "更新实时值";
+            this.realupdate_btn.UseVisualStyleBackColor = true;
+            this.realupdate_btn.Click += new System.EventHandler(this.realupdate_btn_Click);
+            // 
+            // avgupdate_btn
+            // 
+            this.avgupdate_btn.Location = new System.Drawing.Point(227, 172);
+            this.avgupdate_btn.Name = "avgupdate_btn";
+            this.avgupdate_btn.Size = new System.Drawing.Size(91, 23);
+            this.avgupdate_btn.TabIndex = 7;
+            this.avgupdate_btn.Text = "更新小时均值";
+            this.avgupdate_btn.UseVisualStyleBackColor = true;
+            this.avgupdate_btn.Click += new System.EventHandler(this.avgupdate_btn_Click);
+            // 
+            // realstatuslabel
+            // 
+            this.realstatuslabel.AutoSize = true;
+            this.realstatuslabel.Location = new System.Drawing.Point(325, 122);
+            this.realstatuslabel.Name = "realstatuslabel";
+            this.realstatuslabel.Size = new System.Drawing.Size(53, 12);
+            this.realstatuslabel.TabIndex = 8;
+            this.realstatuslabel.Text = "状态: 无";
+            // 
+            // avgstatuslabel
+            // 
+            this.avgstatuslabel.AutoSize = true;
+            this.avgstatuslabel.Location = new System.Drawing.Point(325, 177);
+            this.avgstatuslabel.Name = "avgstatuslabel";
+            this.avgstatuslabel.Size = new System.Drawing.Size(53, 12);
+            this.avgstatuslabel.TabIndex = 9;
+            this.avgstatuslabel.Text = "状态: 无";
+            // 
+            // bothupdate_btn
+            // 
+            this.bothupdate_btn.Location = new System.Drawing.Point(227, 223);
+            this.bothupdate_btn.Name = "bothupdate_btn";
+            this.bothupdate_btn.Size = new System.Drawing.Size(144, 23);
+            this.bothupdate_btn.TabIndex = 10;
+            this.bothupdate_btn.Text = "更新实时值和小时均值";
+            this.bothupdate_btn.UseVisualStyleBackColor = true;
+            this.bothupdate_btn.Click += new System.EventHandler(this.bothupdate_btn_Click);
+            // 
+            // stopupdate_btn
+            // 
+            this.stopupdate_btn.Location = new System.Drawing.Point(227, 271);
+            this.stopupdate_btn.Name = "stopupdate_btn";
+            this.stopupdate_btn.Size = new System.Drawing.Size(91, 23);
+            this.stopupdate_btn.TabIndex = 11;
+            this.stopupdate_btn.Text = "停止";
+            this.stopupdate_btn.UseVisualStyleBackColor = true;
+            this.stopupdate_btn.Click += new System.EventHandler(this.stopupdate_btn_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(290, 383);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(137, 12);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "智企信息 版权所有 2015";
+            // 
+            // UpdateprogressBar
+            // 
+            this.UpdateprogressBar.Location = new System.Drawing.Point(65, 346);
+            this.UpdateprogressBar.Name = "UpdateprogressBar";
+            this.UpdateprogressBar.Size = new System.Drawing.Size(588, 23);
+            this.UpdateprogressBar.Step = 1;
+            this.UpdateprogressBar.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 346);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 12);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "进度:";
+            // 
+            // percentagelabel
+            // 
+            this.percentagelabel.AutoSize = true;
+            this.percentagelabel.Location = new System.Drawing.Point(672, 346);
+            this.percentagelabel.Name = "percentagelabel";
+            this.percentagelabel.Size = new System.Drawing.Size(11, 12);
+            this.percentagelabel.TabIndex = 15;
+            this.percentagelabel.Text = "%";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(500, 288);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(146, 49);
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 403);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.percentagelabel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.UpdateprogressBar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.stopupdate_btn);
+            this.Controls.Add(this.bothupdate_btn);
+            this.Controls.Add(this.avgstatuslabel);
+            this.Controls.Add(this.realstatuslabel);
+            this.Controls.Add(this.avgupdate_btn);
+            this.Controls.Add(this.realupdate_btn);
+            this.Controls.Add(this.EnddateTimePicker);
+            this.Controls.Add(this.StartdateTimePicker);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PointListBox);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "PIDataUpdate V 0.1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_FormClosing(object sender, System.Windows.Forms.FormClosingEventArgs e)
+        {
+            System.IAsyncResult rst = BeginInvoke(closedl);
+            EndInvoke(rst);
+
+            if (logt.ThreadState != System.Threading.ThreadState.Unstarted)
+            {
+                logt.Abort();
+            }
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox PointListBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker StartdateTimePicker;
+        private System.Windows.Forms.DateTimePicker EnddateTimePicker;
+        private System.Windows.Forms.Button realupdate_btn;
+        private System.Windows.Forms.Button avgupdate_btn;
+        private System.Windows.Forms.Label realstatuslabel;
+        private System.Windows.Forms.Label avgstatuslabel;
+        private System.Windows.Forms.Button bothupdate_btn;
+        private System.Windows.Forms.Button stopupdate_btn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ProgressBar UpdateprogressBar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label percentagelabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ PIDataUpdate/Form1.cs
@@ -0,0 +1,411 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Configuration;
+using System.Threading;
+
+using static System.Math;
+using PublicLib;
+
+namespace PIDataUpdate
+{
+
+    public partial class Form1 : Form
+    {
+        private Thread logt = null;
+
+        private int? plantid = null;
+        private string[] pipoints = null;
+        private BackgroundWorker bgw = new BackgroundWorker();
+
+        private int cancelationToken;
+        private object omutex = new object();
+
+        public dl desktopdl;
+        public dl_closeform closedl;
+
+        /// <summary>
+        /// initialize
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            //start logger thread
+            logt = new Thread(new ThreadStart(LogThread));
+            logt.Start();
+
+            try
+            {
+                pipoints = ((string)(new AppSettingsReader()).GetValue("pipoint", typeof(string))).Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                PointListBox.Items.AddRange(pipoints);
+                plantid = int.Parse((string)(new AppSettingsReader()).GetValue("plantid", typeof(string)));
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "---" + ex.Message);
+                ExceptionBody eb = new ExceptionBody() { et = ExceptionType.Error, info = "Initialization?" + ex.Message, ts = DateTime.Now };
+                (new PublicLib.Log()).AddExceptionLog(eb, logtype.console);
+            }
+
+            if ((PointListBox.Items.Count == 0) || (pipoints.Length == 0))
+            {
+                InvalidControls();
+                ExceptionBody eb = new ExceptionBody() { et = ExceptionType.Warning, info = "Initialization?" + "配置文件中没有找到PI计量点", ts = DateTime.Now };
+                (new PublicLib.Log()).AddExceptionLog(eb, logtype.console);
+                MessageBox.Show("配置文件中没有找到PI计量点");
+                return;      
+            }
+            if (plantid == null)
+            {
+                InvalidControls();
+                ExceptionBody eb = new ExceptionBody() { et = ExceptionType.Warning, info = "Initialization?" + "未能初始化plantid", ts = DateTime.Now };
+                (new PublicLib.Log()).AddExceptionLog(eb, logtype.console);
+                MessageBox.Show("未能初始化plantid, 检查配置文件");
+                return;
+            }
+
+            //connect pi
+            new PI.PIFunc2((string)(new AppSettingsReader()).GetValue("ip", typeof(string)), (string)(new AppSettingsReader()).GetValue("username", typeof(string)), (string)(new AppSettingsReader()).GetValue("password", typeof(string)));
+            //connect sql
+            {
+                //no special initialization
+            }
+            //backgroudworker init
+            {
+                bgw.DoWork += Bgw_DoWork;
+                bgw.ProgressChanged += Bgw_ProgressChanged;
+                bgw.RunWorkerCompleted += Bgw_RunWorkerCompleted;
+                bgw.WorkerReportsProgress = true;
+                bgw.WorkerSupportsCancellation = true;
+                desktopdl += updatedesktop;
+                UpdateprogressBar.Style = ProgressBarStyle.Blocks;
+            }
+            //other init
+            {
+                closedl += closeform;
+                this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+                this.FormClosing += Form1_FormClosing;
+            }
+            StartValid();
+        }
+        #region invalidcontrols start_valid start_invalid
+        private void InvalidControls()
+        {
+            avgupdate_btn.Enabled = false;
+            bothupdate_btn.Enabled = false;
+            realupdate_btn.Enabled = false;
+            stopupdate_btn.Enabled = false;
+        }
+
+        private void StartValid()
+        {
+            avgupdate_btn.Enabled = true;
+            bothupdate_btn.Enabled = true;
+            realupdate_btn.Enabled = true;
+            stopupdate_btn.Enabled = false;
+        }
+
+        private void StartInvalid()
+        {
+            avgupdate_btn.Enabled = false;
+            bothupdate_btn.Enabled = false;
+            realupdate_btn.Enabled = false;
+            stopupdate_btn.Enabled = true;
+        }
+        #endregion
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Bgw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                MessageBox.Show("过程提前结束");
+                StartValid();
+            }
+            else
+            {
+                MessageBox.Show("过程已完成");
+                StartValid();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Bgw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (this.IsHandleCreated)
+                this.Invoke(desktopdl, new object[] { e.ProgressPercentage, e.UserState });
+        }
+
+
+        /// <summary>
+        /// real minitues' values
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void realupdate_btn_Click(object sender, EventArgs e)
+        {
+            StartInvalid();
+            //calculate the amout of calculation
+            WorkArgs wa = calculateamout(worktype.real);
+            bgw.RunWorkerAsync(wa);
+
+        }
+
+        /// <summary>
+        /// avg hours' value
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void avgupdate_btn_Click(object sender, EventArgs e)
+        {
+            StartInvalid();
+            //calculate the amout of calculation
+            WorkArgs wa = calculateamout(worktype.avg);
+            bgw.RunWorkerAsync(wa);
+        }
+
+        /// <summary>
+        /// both of above two values
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bothupdate_btn_Click(object sender, EventArgs e)
+        {
+            StartInvalid();
+            //calculate the amout of calculation
+            WorkArgs wa = calculateamout(worktype.both);
+            bgw.RunWorkerAsync(wa);
+        }
+
+        /// <summary>
+        /// stop the process
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void stopupdate_btn_Click(object sender, EventArgs e)
+        {
+            lock (omutex)
+            {
+                if (cancelationToken != 1)
+                    cancelationToken = 1;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Bgw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            WorkArgs warg = (WorkArgs)e.Argument;
+            if (warg.wt == worktype.real)
+            {
+                RealFunc(sender, e);
+            }
+            else if (warg.wt == worktype.avg)
+            {
+                AvgFunc(sender, e);
+            }
+            else if (warg.wt == worktype.both)
+            {
+                RealFunc(sender, e);
+                AvgFunc(sender, e);
+            }
+        }
+        
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private WorkArgs calculateamout(worktype wt)
+        {
+            //will modify later
+            return new WorkArgs() { currentdone = 0, total = 100, wt = wt };
+        }
+
+        /// <summary>
+        /// delegate method for update desktop info
+        /// </summary>
+        /// <param name="pecentage"></param>
+        /// <param name="cs"></param>
+        private void updatedesktop(int percentage, CurrentState cs)
+        {
+            UpdateprogressBar.Value = percentage;
+            percentagelabel.Text = string.Format("{0}%", percentage.ToString());
+            int tempcancellation;
+            lock (omutex)
+            {
+                tempcancellation = cancelationToken;
+            }
+            if (tempcancellation == 1)
+            {
+                bgw.CancelAsync();
+                lock (omutex)
+                {
+                    if (cancelationToken != 0)
+                        cancelationToken = 0;
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// delegate method for closing
+        /// </summary>
+        /// <returns></returns>
+        private int closeform()
+        {
+            stopupdate_btn_Click(null, null);
+            #region
+            //int exited;
+            //while (true)
+            //{
+            //    lock (omutex2)
+            //    {
+            //        exited = appExit;
+            //    }
+            //    if (exited == 0)
+            //        Thread.Sleep(100);
+            //    else
+            //    {
+            //        Thread.Sleep(5000);
+            //        break;
+            //    }
+            //}
+            #endregion
+            Thread.Sleep(2000);
+            return 1;
+        }
+
+        /// <summary>
+        /// real value backgroud func
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RealFunc(object sender, DoWorkEventArgs e)
+        {
+            WorkArgs warg = (WorkArgs)e.Argument;
+            //the line below will be moved
+            warg.total = 100;
+            while (true)
+            {
+                if (((BackgroundWorker)sender).CancellationPending)
+                {
+                    //set cancelled true
+                    e.Cancel = true;
+                    return;
+                }
+
+                Thread.Sleep(100);
+                if (warg.currentdone == 100)
+                    return;
+
+                warg.currentdone++;
+                bgw.ReportProgress((int)warg.currentdone, new CurrentState());
+                //bgw.ReportProgress((int)Math.Floor((double)(warg.currentdone / warg.total * 100)), new CurrentState());
+            }
+        }
+
+        /// <summary>
+        /// avg value background func
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AvgFunc(object sender, DoWorkEventArgs e)
+        {
+            WorkArgs warg = (WorkArgs)e.Argument;
+            //the line below will be moved
+            warg.total = 100;
+            while (true)
+            {
+                if (((BackgroundWorker)sender).CancellationPending)
+                {
+                    //set cancelled true
+                    e.Cancel = true;
+                    return;
+                }
+
+                Thread.Sleep(100);
+                if (warg.currentdone == 100)
+                    return;
+
+                warg.currentdone++;
+                bgw.ReportProgress((int)warg.currentdone, new CurrentState());
+                //bgw.ReportProgress((int)Math.Floor((double)(warg.currentdone / warg.total * 100)), new CurrentState());
+            }
+        }
+
+        /// <summary>
+        /// logger thread
+        /// </summary>
+        private void LogThread()
+        {
+            while (1 == 1)
+            {
+                (new PublicLib.Log()).ExportConsoleLog();
+                Thread.Sleep(500);
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="i"></param>
+    /// <param name="cs"></param>
+    public delegate void dl(int i, CurrentState cs);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public delegate int dl_closeform();
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum worktype
+    {
+        real,
+        avg,
+        both
+    }
+
+    /// <summary>
+    /// arguments for calculating work progress
+    /// </summary>
+    public class WorkArgs
+    {
+        public long total { get; set; } = 0;
+        public long currentdone { get; set; } = 0;
+        public worktype wt { get; set; }
+    }
+
+    /// <summary>
+    /// 当前的线程获取值
+    /// </summary>
+    public class CurrentState
+    {
+        public string pointname { get; set; }
+        public DateTime ts { get; set; }
+        public double pv { get; set; }
+    }
+}
